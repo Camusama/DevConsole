f5a143d043c23abbb5f3a4183cb398f9cdec278c
camusama <marquez.yang@foxmail.com>
2025-04-30 00:55:56 +0800

    chore: pages deploy

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/nextjs'
 export default function Page() {
   return (
     <div className="flex justify-center mt-25">
-      <SignIn />
+      <SignIn oauthFlow="popup" />
     </div>
   )
 }
